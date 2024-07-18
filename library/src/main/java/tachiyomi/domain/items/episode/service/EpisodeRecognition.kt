be8e592b833a07bac947f4ee965156ce463a8583
Secozzi <folke.steen85@gmail.com>
2024-07-18 14:46:02 +0200

    Remove `EpisodeRecognition`

--- library/src/main/java/tachiyomi/domain/items/episode/service/EpisodeRecognition.kt
+++
@@ -1,9 +0,0 @@
-package tachiyomi.domain.items.episode.service
-
-object EpisodeRecognition {
-    fun parseEpisodeNumber(
-        animeTitle: String,
-        episodeName: String,
-        episodeNumber: Double? = null,
-    ): Double = throw Exception("Stub!")
-}
