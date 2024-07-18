d2f96c6ebb27915a919298b5bdd62d04f8264e56
Secozzi <49240133+Secozzi@users.noreply.github.com>
2024-07-18 10:14:58 +0000

    Add `EpisodeRecognition` (#3)

---
+++ library/src/main/java/tachiyomi/domain/items/episode/service/EpisodeRecognition.kt
@@ -0,0 +1,9 @@
+package tachiyomi.domain.items.episode.service
+
+object EpisodeRecognition {
+    fun parseEpisodeNumber(
+        animeTitle: String,
+        episodeName: String,
+        episodeNumber: Double? = null,
+    ): Double = throw Exception("Stub!")
+}
